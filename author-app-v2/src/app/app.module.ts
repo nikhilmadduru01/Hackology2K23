909523dd75cf89b493f9b39263922033d6741218
Harsha_Chalasani <schalasani@uworld.in>
2023-12-14 18:04:30 +0530

    publisher app commit-2

--- author-app-v2/src/app/app.module.ts
+++ author-app-v2/src/app/app.module.ts
@@ -1,17 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
     MatFormFieldModule
   ],
   providers: [],
