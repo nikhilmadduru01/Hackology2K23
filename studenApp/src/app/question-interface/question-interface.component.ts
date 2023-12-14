d0fff8a0945039a21b5766723bd44755583e0a4e
Nagalakshmi Lanka <nlanka@azure.uworld.com>
2023-12-14 17:31:33 +0530

    initial commit

---
+++ studenApp/src/app/question-interface/question-interface.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-question-interface',
+  templateUrl: './question-interface.component.html',
+  styleUrls: ['./question-interface.component.scss']
+})
+export class QuestionInterfaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
